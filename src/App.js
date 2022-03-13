16dc36c86423b67b9cea096f2bf702628ff38a99
Soohyun-Han <soohyun9903@naver.com>
2022-03-13 16:25:48 +0900

    Study0213

--- src/App.js
+++ src/App.js
@@ -2,76 +2,77 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-/*function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}*/
-
-class App extends Component {
+class Subject extends Component {
     render() {
         return (
-            <div className='App'>
-                <div class="wrap">
-        <header>
-            <h1 class="title">SFaCo</h1>
-            <nav>
-                <ul class="menu1">
-                    <li>
-                        전체
-                    </li>
-                    <li>
-                        커뮤니티
-                    </li>
+            <header>
+                <h1 class="title">SFaCo</h1>
+                <nav>
+                    <ul class="menu1">
+                        <li>
+                            전체
+                        </li>
+                        <li>
+                            커뮤니티
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        );
+
+    }
+}
+
+class TOC1 extends Component {
+    render() {
+        return (
+            <section class="menu2">
+                <ul>
+                    <li>공항패션</li>
+                    <li>광고/영화</li>
+                    <li>드라마</li>
+                    <li>예능</li>
+                    <li>기타</li>
                 </ul>
-            </nav>
-        </header>
-        <section class="menu2">
-            <ul>
-                <li>
-                    공항패션
-                </li>
-                <li>
-                    광고/영화
-                </li>
-                
-                <li>
-                    드라마
-                </li>
-                <li>
-                    예능
-                </li>
-                <li>
-                    기타
-                </li>
-            </ul>
-        </section>
-        <section>
+            </section>
+        );
+    }
+}
+
+class TOC2 extends Component {
+    render() {
+        return (
+            <section>
             검색창
-        </section>
-        <footer>
+            </section>
+        )
+    }
+}
+
+class Content extends Component {
+    render(){
+        return (
+            <footer>
             <ul class="menu3">
-                <li><a href="Login.js">로그인</a></li>
+                <li><a href="Login.html">로그인</a></li>
                 <li><a href="MyPage.html">마이페이지</a></li>
             </ul>
             <p>
                 책임자 : 원동연, 한수현 | 서비스문의 : e-mail@naver.com
             </p>
         </footer>
-    </div>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Subject></Subject>
+                <TOC1></TOC1>
+                <TOC2></TOC2>
+                <Content></Content>
             </div>
         )
     }
